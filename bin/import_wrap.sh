149b598d67f784bc4663b04504c8cbfec8cd7fa5
erik <erik@negahok.se>
2014-03-06 12:54:05 +0100

    path fix

--- bin/import_wrap.sh
+++ bin/import_wrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/local/pcaper/bin/import_pcaps.rb -v -d /ncc/pcap/%Y/%m/%d /ncc/pcap/ > /var/log/import_pcaps.log 2>&1
+/usr/local/pcaper/bin/import_pcaps.rb -v -d /opt/pcap/%Y/%m/%d /opt/pcap/ > /var/log/import_pcaps.log 2>&1
 /usr/local/pcaper/bin/generate_argus.rb -v >> /var/log/import_pcaps.log 2>&1
